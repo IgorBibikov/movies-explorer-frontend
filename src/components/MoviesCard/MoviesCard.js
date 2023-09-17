cb21b0f6e4974a79a6f10c214543d186ea70a08b
Igor Bibikov <igb604@yandex.ru>
2023-09-17 19:13:13 +0500

    внесены корректровки2

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -2,19 +2,11 @@ import img from '../../images/mov-img.png';
 function MoviesCard(props) {
   return (
     <article className="movies-card">
-      <button type="button" className="movies-card__save-btn" type="button">
+      <button type="button" className="movies-card__save-btn">
         Сохранить
       </button>
-      <button
-        type="button"
-        className="movies-card__saved-btn"
-        type="button"
-      ></button>
-      <button
-        type="button"
-        className="movies-card__delete-btn"
-        type="button"
-      ></button>
+      <button type="button" className="movies-card__saved-btn"></button>
+      <button type="button" className="movies-card__delete-btn"></button>
       <img className="movies-card__image" src={img} alt={props.card} />
       <div className="movies-card__group">
         <h2 className="movies-card__title">33 слова о дизайне33</h2>
